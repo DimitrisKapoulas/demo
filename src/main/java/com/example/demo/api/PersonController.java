26ad03a737f0165ef647301525ec76b645db781f
DimitrisKapoulas <kapoulasd@gmail.com>
2019-09-02 11:49:18 +0300

    Adding 2 more annotations, plus working on the POST method

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -3,7 +3,10 @@ package com.example.demo.api;
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class PersonController {
 
     private final PersonService personService;
@@ -13,6 +16,7 @@ public class PersonController {
         this.personService = personService;
     }
 
+    @PostMapping
     public void addPerson(Person person) {
         personService.addPerson(person);
     }
